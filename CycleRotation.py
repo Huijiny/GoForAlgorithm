29743f1c00ca0bf6eaf00c05f86d2ddca6be8a1b
Huijiny <jiin20803@gmail.com>
2020-09-12 23:50:58 +0900

    move folder

--- CycleRotation.py
+++
@@ -1,9 +0,0 @@
-def solution(A, K):
-    l = len(A) #4
-    for _ in range(K):
-        B = list(0 for i in range(l))
-        for i, num in enumerate(A):
-            B[i+1-l] = num # 3+1-len(A)
-        A = B    
-    
-return A
--- FrogJmp.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def solution(X,Y,D):
-    return math.ceil((Y-X)/D)
-
-print(solution(0,20,2.5))
-
